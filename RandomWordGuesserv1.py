db72bfbeab2e5c322f071494c71e3f237d0a0023
Account-95555 <100207649+Account-95555@users.noreply.github.com>
2022-05-25 09:22:02 +0800

    Update RandomWordGuesserv1.py
    comments...

--- RandomWordGuesserv1.py
+++ RandomWordGuesserv1.py
@@ -4,6 +4,7 @@
 #Made by Account95555
 
 import random
+#this list took up 1/5 of the programming time
 alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 playagain = True
 
